80b7c954f8bd92f34e131e18ffa7338bb2b8f542
priya28200 <160150673+priya28200@users.noreply.github.com>
2024-02-25 19:31:04 +0530

    Create 01.madlibs.py

---
+++ 01.madlibs.py
@@ -0,0 +1,8 @@
+adj = input("Adjective:")
+noun1 = input("Noun:")
+noun2 = input("Noun:")
+verb1 = input("Verb:")
+verb2 = input("Verb:")
+madlibs = f"The {noun1} decided to {verb1} in a {adj} manner. Suddenly, a wild {noun2} appeared and challenged it to a {verb2} competition."
+
+print(madlibs)
